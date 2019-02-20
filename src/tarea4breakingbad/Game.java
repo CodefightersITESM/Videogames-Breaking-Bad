adf17c7636cf887274df7d96dad74a6bd22bcd00
Isabel Cruz <isabel.cruz81699@gmail.com>
2019-02-20 11:37:19 -0600

    fixed

--- src/tarea4breakingbad/Game.java
+++ src/tarea4breakingbad/Game.java
@@ -102,21 +102,15 @@ public class Game implements Runnable {
     /**
      * Starts all initializations needed to start the game.
      */
-    private void init() {
-        // initialize the game's assets
-        Assets.init();
-        
-        // start the game
-        initItems();
-        
-         // create the display
+    private void init() {      
+        // create the display
         display = new Display(getTitle(), getWidth(), getHeight());
         display.getWindow().addKeyListener(getKeyManager());
         display.getWindow().addMouseListener(getMouseManager());
         display.getWindow().addMouseMotionListener(getMouseManager());
         display.getCanvas().addMouseListener(getMouseManager());
         display.getCanvas().addMouseMotionListener(getMouseManager());
-
+        
         // initialize the game's assets
         Assets.init();
         
