9fd01af62d2065135a875d291b87cdbb429e05df
Isabel Cruz <isabel.cruz81699@gmail.com>
2019-02-25 17:50:05 -0600

    created player class with item elements

--- src/tarea4breakingbad/Player.java
+++ src/tarea4breakingbad/Player.java
@@ -5,10 +5,28 @@
  */
 package tarea4breakingbad;
 
+import java.awt.Graphics;
+
 /**
  *
  * @author Cesar Barraza
+ * @author Isabel Cruz
  */
-public class Player {
+public class Player extends Item {
+    private Game game;
+    
+    public Player(int x, int y, int width, int height, Game game){
+        super(x,y,width,height);
+        this.game = game;
+    }
+    
+    @Override
+    public void update() {
+        
+    }
+
+    @Override
+    public void render(Graphics g) {
+    }
     
 }
